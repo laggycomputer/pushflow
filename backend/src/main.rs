5d7ed9d582377441ade0be6baa6053667396f370
Dante Dam <laggycomputer@yahoo.com>
2025-08-06 23:52:03 -0700

    first half of oauth is done

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,22 +1,74 @@
-use actix_web::web::Path;
-use actix_web::{get, App, HttpServer, Responder};
-use anyhow::Context;
+mod oauth;
 
-#[get("/hello/{name}")]
-async fn greet(name: Path<String>) -> impl Responder {
-    format!("Hello {}!", name)
+use crate::oauth::{oauth_cb_goog, oauth_start_goog, GoogleOAuthConfig, OAuth};
+use actix_web::{App, HttpServer, ResponseError};
+use anyhow::Context;
+use std::ffi::OsString;
+use std::fmt::{Display, Formatter};
+
+#[repr(transparent)]
+#[derive(Debug)]
+struct AnyhowBridge(anyhow::Error);
+
+impl Display for AnyhowBridge {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl<T> From<T> for AnyhowBridge
+    where T: Into<anyhow::Error> {
+    fn from(value: T) -> Self {
+        Self(value.into())
+    }
+}
+
+type Result<T> = std::result::Result<T, AnyhowBridge>;
+
+impl ResponseError for AnyhowBridge {
+    // TODO: actual error contents
+}
+
+#[derive(Clone, Debug)]
+struct AppData {
+    client: reqwest::Client,
+    oauth: OAuth,
+    jwt_secret: Box<[u8]>,
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenv::dotenv().ok().context("read dotenv")?;
+
     let port = std::env::var("PORT")
         .unwrap_or(String::from("1451"))
         .parse::<u16>()
         .context("$PORT not valid u16 port")?;
 
-    let server = HttpServer::new(|| App::new().service(greet))
+    let app_data = Box::leak(Box::new(AppData {
+        client: reqwest::Client::new(),
+        oauth: OAuth {
+            google: GoogleOAuthConfig {
+                client_id: std::env::var("GOOGLE_OAUTH_CLIENT_ID")
+                    .context("read env var GOOGLE_OAUTH_CLIENT_ID")?,
+                client_secret: std::env::var("GOOGLE_OAUTH_CLIENT_SECRET")
+                    .context("read env var GOOGLE_OAUTH_CLIENT_SECRET")?,
+            },
+        },
+        jwt_secret: Box::from(std::env::var_os("JWT_SECRET")
+            .unwrap_or(OsString::from("0f3c13e6a2fc1e6ed08ed391de5e89276f72bb3a")).as_encoded_bytes()
+)    }));
+
+    let server = {
+        HttpServer::new(|| {
+            App::new()
+                .app_data(&*app_data)
+                .service(oauth_start_goog)
+                .service(oauth_cb_goog)
+        })
         .bind(("127.0.0.1", port))
-        .with_context(|| format!("bind to port {port}"))?;
+        .with_context(|| format!("bind to port {port}"))
+    }?;
 
     eprintln!("ok, alive on port {port}...");
 
---
+++ backend/src/oauth.rs
@@ -0,0 +1,83 @@
+use crate::AppData;
+use actix_web::cookie::{Cookie, SameSite};
+use actix_web::web::Query;
+use actix_web::{cookie, get, HttpRequest, HttpResponse, Responder};
+use anyhow::Context;
+use jsonwebtoken::Header;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug)]
+pub(crate) struct OAuth {
+    pub(crate) google: GoogleOAuthConfig,
+}
+
+#[derive(Clone, Debug)]
+pub(crate) struct GoogleOAuthConfig {
+    pub(crate) client_id: String,
+    pub(crate) client_secret: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct GoogleOAuthJWT {
+    // this is supposed to be a UUIDv4
+    state: String,
+}
+
+#[get("/oauth/start/goog")]
+async fn oauth_start_goog(req: HttpRequest) -> crate::Result<impl Responder> {
+    let data = *req.app_data::<&AppData>().unwrap();
+
+    let state = uuid::Uuid::new_v4();
+
+    let goog_response = data.client
+        .execute(
+            data.client
+                .get("https://accounts.google.com/o/oauth2/v2/auth")
+                .query(&[
+                    ("client_id", &*data.oauth.google.client_id),
+                    // TODO: this redirect will be in the JS backend
+                    ("redirect_uri", "http://localhost:1451/oauth/cb/goog"),
+                    ("response_type", "code"),
+                    ("state", state.to_string().as_str()),
+                    ("scope", "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"),
+                ])
+                .build()?,
+        )
+        .await
+        .context("init oauth with goog")?;
+
+    let jwt = GoogleOAuthJWT {
+        state: state.to_string(),
+    };
+
+    let encoded = jsonwebtoken::encode(&Header::default(), &jwt, &jsonwebtoken::EncodingKey::from_secret(&*data.jwt_secret))
+        .context("build JWT token")?;
+
+    // give back the google URL and the state
+
+    Ok(HttpResponse::Ok()
+        .cookie(Cookie::build("oauth_state", encoded.to_string())
+            .max_age(cookie::time::Duration::minutes(5))
+            // not defaulted on firefox and safari
+            .same_site(SameSite::Lax)
+            .finish()
+        )
+        .body(goog_response.url().as_str().to_owned()))
+}
+
+// JS backend will give us the query params unchanged
+#[derive(Debug, Deserialize)]
+struct OAuthCbGoogQuery {
+    error: Option<String>,
+    code: Option<String>,
+}
+
+#[get("/oauth/cb/goog")]
+async fn oauth_cb_goog(info: Query<OAuthCbGoogQuery>) -> impl Responder {
+    dbg!(info);
+    // Google redirects user to /oauth/cb/goog?code=xxxxxxx&state=xxxxxx&...
+    // client has their "correct state" in the signed cookie
+    // check that the state you get from state=xxxxx si the same as the signed goo
+
+    "hi"
+}
