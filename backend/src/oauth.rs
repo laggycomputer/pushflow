880557d31d8167b1e9a8041528072e15326cded2
Dante Dam <laggycomputer@yahoo.com>
2025-08-07 00:29:11 -0700

    return types fix

--- backend/src/oauth.rs
+++ backend/src/oauth.rs
@@ -2,6 +2,7 @@ use crate::AppData;
 use actix_web::cookie::{Cookie, SameSite};
 use actix_web::web::Query;
 use actix_web::{HttpRequest, HttpResponse, Responder, cookie, get};
+use actix_web::error::ParseError::Status;
 use actix_web::http::StatusCode;
 use anyhow::Context;
 use jsonwebtoken::{DecodingKey, Header, Validation};
@@ -81,16 +82,16 @@ struct OAuthCbGoogQuery {
 }
 
 #[get("/oauth/cb/goog")]
-async fn oauth_cb_goog(info: Query<OAuthCbGoogQuery>, req: HttpRequest) -> impl Responder {
+async fn oauth_cb_goog(info: Query<OAuthCbGoogQuery>, req: HttpRequest) -> crate::Result<impl Responder> {
     let data = *req.app_data::<&AppData>().unwrap();
 
     // client has their "correct state" in the signed cookie
     let token = jsonwebtoken::decode::<GoogleOAuthJWT>(match req.cookie("oauth_state") {
-        None => return ("no state", StatusCode::BAD_REQUEST),
+        None => return Ok(("no state", StatusCode::BAD_REQUEST)),
         Some(state) => state.value()
     }, &DecodingKey::from_secret(&*data.jwt_secret), &Validation::default())?;
 
     // need to give JS side user profile URL and email
 
-    "hi"
+    Ok(("hi", StatusCode::OK))
 }
