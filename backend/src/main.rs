3c2421d3ec339e6d74d084509574941fbee48597
Dante Dam <laggycomputer@yahoo.com>
2025-08-06 19:03:03 -0700

    i lied use actix

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,58 +1,24 @@
+use actix_web::web::Path;
+use actix_web::{get, App, HttpServer, Responder};
 use anyhow::Context;
-use http_body_util::Full;
-use hyper::body::Bytes;
-use hyper::Response;
-use hyper_util::rt::TokioIo;
-use std::convert::Infallible;
-use std::net::SocketAddr;
-use hyper::service::service_fn;
-use tokio::net::TcpListener;
 
-#[derive(Clone)]
-pub struct Executor;
-
-impl<F> hyper::rt::Executor<F> for Executor
-where F: Future + Send + 'static,
-    F::Output: Send + 'static,
-{
-    fn execute(&self, fut: F) {
-        tokio::spawn(fut);
-    }
-}
-
-async fn hello(_: hyper::Request<hyper::body::Incoming>) -> Result<Response<Full<Bytes>>, Infallible> {
-    Ok(Response::new(Full::new(Bytes::from("Hello, World!"))))
+#[get("/hello/{name}")]
+async fn greet(name: Path<String>) -> impl Responder {
+    format!("Hello {}!", name)
 }
 
 #[tokio::main]
-async fn main() -> Result<(), anyhow::Error> {
+async fn main() -> anyhow::Result<()> {
     let port = std::env::var("PORT")
         .unwrap_or(String::from("1451"))
         .parse::<u16>()
         .context("$PORT not valid u16 port")?;
 
-    let listener = TcpListener::bind(SocketAddr::from((
-        [127, 0, 0, 1],
-        port,
-    ))).await.with_context(|| format!("bind to port {port}"))?;
+    let server = HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", port))
+        .with_context(|| format!("bind to port {port}"))?;
 
-    eprintln!("ok, alive on {}...", listener.local_addr()?);
+    eprintln!("ok, alive on port {port}...");
 
-    loop {
-        let (stream, _) = listener.accept().await.context("await new conn")?;
-        let io = TokioIo::new(stream);
-
-        tokio::task::spawn(async move {
-             // Handle the connection from the client using HTTP/2 with an executor and pass any
-            // HTTP requests received on that connection to the `hello` function
-            if let Err(err) = hyper::server::conn::http1::Builder::new()
-                .serve_connection(io, service_fn(hello))
-                .await
-            {
-                eprintln!("Error serving connection: {}", err);
-            }
-        });
-    }
-
-    Ok(())
+    Ok(server.run().await?)
 }
