7ccdaef23424a1d6f8397c81dd4c40836ac3a810
Dante Dam <laggycomputer@yahoo.com>
2025-08-09 10:54:06 -0700

    start on services

--- backend/src/gated/service.rs
+++ backend/src/gated/service.rs
@@ -1,10 +1,24 @@
 use crate::ExtractedAppData;
+use crate::gated::SessionUser;
 use actix_session::Session;
-use actix_web::{get, Responder};
+use actix_web::{Responder, get};
+use anyhow::Context;
+use entity::services;
+use sea_orm::ColumnTrait;
+use sea_orm::EntityTrait;
+use sea_orm::QueryFilter;
 
-#[get("/")]
+#[get("")]
 async fn service(data: ExtractedAppData, session: Session) -> crate::Result<impl Responder> {
+    let session_user = session
+        .get::<SessionUser>("user")?
+        .context("no session user")?;
 
-
+    dbg!(
+        services::Entity::find()
+            .filter(services::Column::OwnerId.eq(session_user.user_id))
+            .all(&data.db)
+            .await?
+    );
     Ok("todo")
 }
