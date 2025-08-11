96513fadb351794e512cfe03560017b28803b9ae
Awesome-E <54484616+Awesome-E@users.noreply.github.com>
2025-08-11 01:42:04 -0700

    log failing urls

--- frontend/src/app/api/login/route.ts
+++ frontend/src/app/api/login/route.ts
@@ -3,7 +3,10 @@ import { cookies } from "next/headers"
 import { NextResponse } from "next/server"
 
 export async function GET () {
-  const result = await fetch(config.BACKEND_URL + '/oauth/start/goog')
+  const url = config.BACKEND_URL + '/oauth/start/goog'
+  const result = await fetch(url)
+    .catch(err => console.error(url, err))
+  if (!result) return NextResponse.json(null, { status: 500 })
   const stateCookie = result.headers.getSetCookie().find(c => c.startsWith('oauth_state'))!
   const cookieStore = await cookies()
   cookieStore.set('oauth_state', stateCookie)
